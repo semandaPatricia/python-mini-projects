58dc5454868274b3d03213313f04d66899c60438
semanda <patash248@gmail.com>
2024-01-26 13:20:07 +0100

    turtle graphics

---
+++ turtle_racing.py
@@ -0,0 +1,10 @@
+import turtle
+
+scr = turtle.Screen()
+scr.bgcolor("cyan")
+scr.title("Board Infinity")
+tur = turtle.Turtle()
+
+tur.forward(200)
+
+turtle.done()
---
+++ turtleracing.py
@@ -0,0 +1,132 @@
+"""
+
+import turtle
+import random
+import time
+
+scr = turtle.Screen()
+scr.bgcolor("cyan")
+scr.title("Turtle racing")
+racer = turtle.Turtle()
+
+
+WIDTH, HEIGHT = 700, 600
+COLORS = ['red', 'green', 'blue', 'orange', 'yellow', 'black', 'purple', 'pink', 'brown', 'cyan']
+scr.setup(WIDTH, HEIGHT)
+
+def get_number_of_racers():
+	racers = 0
+	while True:
+		racers = input('Enter the number of racers (2 - 10): ')
+		if racers.isdigit():
+			racers = int(racers)
+		else:
+			print('Input is not numeric... Try Again!')
+			continue
+
+		if 2 <= racers <= 10:
+			return racers
+		else:
+			print('Number not in range 2-10. Try Again!')
+
+def race(colors):
+	turtles = create_turtles(colors)
+
+	while True:
+		for racer in turtles:
+			distance = random.randrange(1, 20)
+			racer.forward(distance)
+
+			x, y = racer.pos()
+			if y >= HEIGHT // 2 - 10:
+				return colors[turtles.index(racer)]
+
+
+
+def create_turtles(colors):
+	turtles = []
+	spacingx = WIDTH // (len(colors) + 1)
+	for i, color in enumerate(colors):
+		racer = turtle.Turtle()
+		racer.color(color)
+		racer.shape('turtle')
+		racer.left(90)
+		racer.penup()
+		racer.setpos(-WIDTH//2 + (i + 1) * spacingx, -HEIGHT//2 + 20)
+		racer.pendown()
+		turtles.append(racer)
+
+	return turtles
+
+def init_turtle():
+	screen = turtle.Screen()
+	screen.setup(WIDTH, HEIGHT)
+	screen.title('Turtle Racing!')
+
+racers = get_number_of_racers()
+init_turtle()
+
+random.shuffle(COLORS)
+colors = COLORS[:racers]
+
+"""
+
+import turtle
+import random
+
+
+WIDTH, HEIGHT = 700, 600
+COLORS = ['red', 'green', 'blue', 'orange', 'yellow', 'black', 'purple', 'pink', 'brown', 'cyan']
+
+def get_number_of_racers():
+    racers = 0
+    while True:
+        racers = input('Enter the number of racers (2 - 10): ')
+        if racers.isdigit():
+            racers = int(racers)
+        else:
+            print('Input is not numeric... Try Again!')
+            continue
+
+        if 2 <= racers <= 10:
+            return racers
+        else:
+            print('Number not in the range 2-10. Try Again!')
+
+def race(colors):
+    turtles = create_turtles(colors)
+    max_steps = 100
+
+    for step in range(max_steps):
+        for racer in turtles:
+            distance = random.randrange(1, 20)
+            racer.forward(distance)
+
+            x, y = racer.pos()
+            if y >= HEIGHT // 2 - 10:
+                return colors[turtles.index(racer)]
+
+def create_turtles(colors):
+    turtles = []
+    spacingx = WIDTH // (len(colors) + 1)
+    for i, color in enumerate(colors):
+        racer = turtle.Turtle()
+        racer.color(color)
+        racer.shape('turtle')
+        racer.left(90)
+        racer.penup()
+        racer.setpos(-WIDTH//2 + (i + 1) * spacingx, -HEIGHT//2 + 20)
+        racer.pendown()
+        turtles.append(racer)
+
+    return turtles
+
+racers = get_number_of_racers()
+random.shuffle(COLORS)
+colors = COLORS[:racers]
+
+winner_color = race(colors)
+print(f"The {winner_color} turtle wins the race!")
+turtle.done()
+
+
